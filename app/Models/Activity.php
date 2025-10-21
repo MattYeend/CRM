1c1a2ddf8cd77a5ae5e6acc09e2748ebbe6203e9
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-10-21 15:43:10 +0100

    #19 New activities model and migration

---
+++ app/Models/Activity.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['user_id','type','description','meta'];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    /**
+     * Get the user that owns the activity.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the owning subject model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function subject()
+    {
+        return $this->morphTo();
+    }
+}
