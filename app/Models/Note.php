c0243778c69ffb04ff2fabb86cc1e98e2367b9c6
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-10-21 15:31:49 +0100

    #18 Note model and migration

---
+++ app/Models/Note.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Note extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['body','user_id','meta'];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    /**
+     * Get the owning notable model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function notable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Get the user that owns the note.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
