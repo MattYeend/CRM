1270183b6e536db3520854160805292d51c886dc
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-10-22 12:29:16 +0100

    #46 New Note factory

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Note;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Note>
+ */
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => null,
+            'body' => $this->faker->paragraph(),
+            'meta' => [],
+        ];
+    }
+
+    /**
+     * Configure the factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterCreating(function (Note $note) {
+            if (!$note->user_id) {
+                $note->user()->associate(User::factory()->create());
+                $note->save();
+            }
+        });
+    }
+}
