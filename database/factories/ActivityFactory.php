b6809ab4f58ec110cbe5ed79e32790e3228cd430
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-10-22 12:30:18 +0100

    #46 New Activity Factory

---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Activity>
+ */
+class ActivityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => null,
+            'type' => 'generic',
+            'description' => $this->faker->optional()->sentence(),
+            'meta' => [],
+        ];
+    }
+
+    /**
+     * Configure the factory.
+     *
+     * @return $this
+     */
+    public function configure()
+    {
+        return $this->afterCreating(function (Activity $activity) {
+            if (!$activity->user_id) {
+                $activity->user()->associate(User::factory()->create());
+                $activity->save();
+            }
+        });
+    }
+}
