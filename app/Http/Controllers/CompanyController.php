ef3ea00d138246cd4cd97ad75d2eb811a19e39a8
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-10-26 12:35:53 +0000

    #52 New company controller

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $perPage = (int) $request->query('per_page', 10);
+        $q = $request->query('q');
+
+        $query = Company::query()->withCount(['contacts', 'deals', 'invoices']);
+
+        if ($q) {
+            $query->where('name', 'like', '%' . $q . '%');
+        }
+
+        return response()->json($query->paginate($perPage));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Company $company
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Company $company)
+    {
+        return response()->json($company->load('contacts','deals','invoices','attachments'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'industry' => 'nullable|string',
+            'website' => 'nullable|url',
+            'phone' => 'nullable|string',
+            'address' => 'nullable|string',
+            'city' => 'nullable|string',
+            'region' => 'nullable|string',
+            'postal_code' => 'nullable|string',
+            'country' => 'nullable|string',
+            'meta' => 'nullable|array',
+        ]);
+
+        $company = Company::create($data);
+        return response()->json($company, 201);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     *
+     * @param Company $company
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, Company $company)
+    {
+        $data = $request->validate([
+            'name' => 'sometimes|required|string',
+            'industry' => 'nullable|string',
+            'website' => 'nullable|url',
+            'phone' => 'nullable|string',
+            'address' => 'nullable|string',
+            'city' => 'nullable|string',
+            'region' => 'nullable|string',
+            'postal_code' => 'nullable|string',
+            'country' => 'nullable|string',
+            'meta' => 'nullable|array',
+        ]);
+
+        $company->update($data);
+        return response()->json($company);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Company $company
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Company $company)
+    {
+        $company->delete();
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Restore the specified resource from soft deletion.
+     *
+     * @param int $id
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function restore($id)
+    {
+        $company = Company::withTrashed()->findOrFail($id);
+        $company->restore();
+        return response()->json($company);
+    }
+}
