f73e14daa639de9863f2b2214c219e0f2498ed13
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-11 15:11:31 +0000

    #85 New User Log Service

---
+++ app/Services/UserLogService.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Log;
+use App\Models\User;
+
+class UserLogService
+{
+    public function __construct()
+    {
+        // Empty constructor
+    }
+
+    /**
+     * Log the creation of a User.
+     *
+     * @param User $actor The user that created the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userCreated(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'created_at' => $user->created_at,
+            'created_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_CREATE_USER,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Log the update of a User.
+     *
+     * @param User $actor The user that updated the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userUpdated(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'updated_at' => $user->updated_at,
+            'updated_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_UPDATE_USER,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Log the deletion of a User.
+     *
+     * @param User $actor The user that deleted the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userDeleted(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'deleted_at' => $user->deleted_at,
+            'deleted_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_DELETE_USER,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Log the verification of a User.
+     *
+     * @param User $actor The user that verified the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userVerified(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'verified_at' => now(),
+            'verified_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_VERIFY_USER,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Log the restoration of a User.
+     *
+     * @param User $actor The user that restored the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userRestored(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'restored_at' => now(),
+            'restored_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_USER_RESTORED,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Log the force deletion of a User.
+     *
+     * @param User $actor The user that force deleted the user.
+     *
+     * @param int $actorId The ID of the user who performed the action.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return Log The created log entry.
+     */
+    public function userForceDeleted(
+        User $actor,
+        int $actorId,
+        User $user
+    ): array {
+        $data = $this->baseUserData($user) + [
+            'force_deleted_at' => now(),
+            'force_deleted_by' => $actor->name,
+        ];
+
+        Log::log(
+            Log::ACTION_USER_FORCED_DELETED,
+            $data,
+            $actorId,
+        );
+
+        return $data;
+    }
+
+    /**
+     * Prepare base user data for logging.
+     *
+     * @param User $user The user being logged.
+     *
+     * @return array The base user data.
+     */
+    protected function baseUserData(User $user): array
+    {
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+            'role' => $user->role->name,
+            'phone' => $user->phone,
+            'avatar' => $user->avatar,
+        ];
+    }
+}
