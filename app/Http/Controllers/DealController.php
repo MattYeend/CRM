4e3e9b9d1836860716695fdc8fe1b76e07d72fcc
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-12 11:31:08 +0000

    #82 Added log services to deal controller

--- app/Http/Controllers/DealController.php
+++ app/Http/Controllers/DealController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Deal;
+use App\Services\DealLogService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DealController extends Controller
 {
+    /**
+     * Declare a protected property to hold the ContactLogService instance
+     *
+     * @var DealLogService
+     */
+    protected DealLogService $logger;
+
+    /**
+     * Constructor for the controller
+     *
+     * @param DealLogService $logger
+     * An instance of the DealLogService used for logging
+     * deal-related actions
+     */
+    public function __construct(DealLogService $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -86,6 +106,12 @@ class DealController extends Controller
 
         $deal = Deal::create($data);
 
+        $this->logger->dealCreated(
+            auth()->user(),
+            auth()->id(),
+            $deal
+        );
+
         return response()->json($deal->load($this->relations()), 201);
     }
 
@@ -100,23 +126,16 @@ class DealController extends Controller
      */
     public function update(Request $request, Deal $deal)
     {
-        $data = $request->validate([
-            'title' => 'sometimes|required|string',
-            'company_id' => 'nullable|integer|exists:companies,id',
-            'contact_id' => 'nullable|integer|exists:contacts,id',
-            'owner_id' => 'nullable|integer|exists:users,id',
-            'pipeline_id' => 'nullable|integer|exists:pipelines,id',
-            'stage_id' => 'nullable|integer|exists:pipeline_stages,id',
-            'value' => 'nullable|numeric',
-            'currency' => 'nullable|string|max:8',
-            'close_date' => 'nullable|date',
-            'status' => 'nullable|in:open,won,lost,archived',
-            'meta' => 'nullable|array',
-        ]);
+        $data = $this->validateUpdateData($request);
 
         DB::transaction(function () use ($deal, $data) {
             $deal->update($data);
         });
+
+        $this->logUpdate(
+            $request,
+            $deal
+        );
 
         return response()->json($deal->fresh()->load($this->relations()));
     }
@@ -131,6 +150,13 @@ class DealController extends Controller
     public function destroy(Deal $deal)
     {
         $deal->delete();
+
+        $this->logger->dealDeleted(
+            auth()->user(),
+            auth()->id(),
+            $deal
+        );
+
         return response()->json(null, 204);
     }
 
@@ -145,6 +171,11 @@ class DealController extends Controller
     {
         $deal = Deal::withTrashed()->findOrFail($id);
         $deal->restore();
+        $this->logger->dealRestored(
+            auth()->user(),
+            auth()->id(),
+            $deal
+        );
         return response()->json($deal);
     }
 
@@ -156,5 +187,46 @@ class DealController extends Controller
     private function relations(): array
     {
         return ['company', 'contact', 'owner', 'pipeline', 'stage'];
+    }
+
+    /**
+     * Validate the update data for a deal.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    private function validateUpdateData(Request $request): array
+    {
+        return $request->validate([
+            'title' => 'sometimes|required|string',
+            'company_id' => 'nullable|integer|exists:companies,id',
+            'contact_id' => 'nullable|integer|exists:contacts,id',
+            'owner_id' => 'nullable|integer|exists:users,id',
+            'pipeline_id' => 'nullable|integer|exists:pipelines,id',
+            'stage_id' => 'nullable|integer|exists:pipeline_stages,id',
+            'value' => 'nullable|numeric',
+            'currency' => 'nullable|string|max:8',
+            'close_date' => 'nullable|date',
+            'status' => 'nullable|in:open,won,lost,archived',
+            'meta' => 'nullable|array',
+        ]);
+    }
+    /**
+     * Log the update of a deal.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @param \App\Models\Deal $deal
+     *
+     * @return void
+     */
+    private function logUpdate(Request $request, $deal): void
+    {
+        $this->logger->dealUpdated(
+            $request->user(),
+            auth()->id(),
+            $deal
+        );
     }
 }
