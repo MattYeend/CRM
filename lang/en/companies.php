c5936ea2ce2e18e130322e465a3a3a57090bcc61
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-26 20:47:32 +0000

    #92 New Companies Language File

---
+++ lang/en/companies.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Companies Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for company-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Company',
+    'update' => 'Update Company',
+    'show' => 'View Company Details',
+    'delete' => 'Delete Company',
+    'list' => 'List of Companies',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'industry' => 'Industry',
+    'website' => 'Website',
+    'phone' => 'Phone',
+    'address' => 'Address',
+    'city' => 'City',
+    'region' => 'Region',
+    'postal_code' => 'Postal Code',
+    'country' => 'Country',
+    'meta' => 'Meta Information',
+    'number_of_employees' => 'Number of Employees',
+];
