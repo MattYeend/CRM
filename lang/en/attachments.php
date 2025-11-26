a6f727ffc12e34eb18c22c3271f68356635bb88b
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-26 17:58:02 +0000

    #92 New Attachment Language File

---
+++ lang/en/attachments.php
@@ -0,0 +1,35 @@
+<?php 
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Attachments Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for attachment-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+    'create' => 'Create an Attachment',
+    'update' => 'Update Attachment',
+    'show' => 'View Attachment Details',
+    'delete' => 'Delete Attachment',
+    'list' => 'List of Attachments',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'uploaded_by' => 'Uploaded By',
+    'disk' => 'Disk',
+    'file_path' => 'File Path',
+    'attachable_type' => 'Attachable Type',
+    'attachable_id' => 'Attachable ID',
+    'size' => 'Size',
+    'mime' => 'MIME Type',
+];
