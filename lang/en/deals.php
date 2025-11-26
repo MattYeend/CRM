1b54e5daeb997e5b22aec874dec78c227a04f170
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-26 21:09:41 +0000

    #92 New Deal Language File

---
+++ lang/en/deals.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Deals Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for deal-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Deal',
+    'update' => 'Update Deal',
+    'show' => 'View Deal Details',
+    'delete' => 'Delete Deal',
+    'list' => 'List of Deals',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'company' => 'Company',
+    'contact' => 'Contact',
+    'owner' => 'Owner',
+    'pipeline' => 'Pipeline',
+    'stage' => 'Stage',
+    'value' => 'Value',
+    'currency' => 'Currency',
+    'close_date' => 'Close Date',
+    'meta' => 'Meta Information',
+];
