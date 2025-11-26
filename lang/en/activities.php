2fd1e1d9e976b412e699c5a31a7e9dc31346405f
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-26 12:25:36 +0000

    #92 New Activity Language File

---
+++ lang/en/activities.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Activities Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for activity-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create an Activity',
+    'update' => 'Update Activity',
+    'show' => 'View Activity Details',
+    'delete' => 'Delete Activity',
+    'list' => 'List of Activities',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'type' => 'Type',
+    'subject_type' => 'Subject Type',
+    'meta' => 'Meta Information',
+];
