6f376ac4fa63e359f170c4bc3c848ef1244f3ab3
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-26 20:51:27 +0000

    #92 New Contacts Language File

---
+++ lang/en/contacts.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Contacts Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for contact-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Contact',
+    'update' => 'Update Contact',
+    'show' => 'View Contact Details',
+    'delete' => 'Delete Contact',
+    'list' => 'List of Contacts',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+    'phone' => 'Phone',
+    'job_title' => 'Job Title',
+    'meta' => 'Meta Information',
+    'company' => 'Company',
+];
