f0029c70b7a9fa03825d4952f9ec6f750b8c503e
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:28:04 +0000

    #92 New Pipeline Stage Language File

---
+++ lang/en/pipelineStages.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pipeline Stages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for pipeline stage-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Pipeline Stage',
+    'update' => 'Update Pipeline Stage',
+    'show' => 'View Pipeline Stage Details',
+    'delete' => 'Delete Pipeline Stage',
+    'list' => 'List of Pipeline Stages',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'pipeline' => 'Pipeline',
+    'position' => 'Position',
+    'is_won_stage' => 'Is Won Stage',
+    'is_lost_stage' => 'Is Lost Stage',
+];
