ba144a07a43a433ae2ecf06f069c3bd719462e87
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:14:52 +0000

    #92 New Notes Language File

---
+++ lang/en/notes.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Notes Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for note-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Note',
+    'update' => 'Update Note',
+    'show' => 'View Note Details',
+    'delete' => 'Delete Note',
+    'list' => 'List of Notes',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'notable_type' => 'Notable Type',
+    'notable_id' => 'Notable ID',
+    'user' => 'User',
+    'body' => 'Body',
+    'meta' => 'Meta Information',
+];
