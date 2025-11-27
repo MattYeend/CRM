e1498833fb0a835bcad9ba239ae9f4bf1652bf5d
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:39:12 +0000

    #92 New User Language File

---
+++ lang/en/users.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Users Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for user-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a User',
+    'update' => 'Update User',
+    'show' => 'View User Details',
+    'delete' => 'Delete User',
+    'list' => 'List of Users',
+    'name' => 'Name',
+    'email' => 'Email',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'roles' => 'Roles',
+    'assign_role' => 'Assign Role',
+    'remove_role' => 'Remove Role',
+    'phone' => 'Phone',
+    'avatar' => 'Avatar',
+];
