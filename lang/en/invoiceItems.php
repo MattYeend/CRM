03f0ea452b814cbc0ccbd507b9e3b586c5eb474a
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 10:07:33 +0000

    #92 New Invoice Item Language File

---
+++ lang/en/invoiceItems.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Invoice Items Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for invoice item-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create an Invoice Item',
+    'update' => 'Update Invoice Item',
+    'show' => 'View Invoice Item Details',
+    'delete' => 'Delete Invoice Item',
+    'list' => 'List of Invoice Items',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'invoice' => 'Invoice',
+    'product' => 'Product',
+    'description' => 'Description',
+    'quantity' => 'Quantity',
+    'unit_price' => 'Unit Price',
+    'line_total' => 'Line Total',
+    'meta' => 'Meta Information',
+];
