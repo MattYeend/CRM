d7f88203f5f9f9fb612d214b9fcb5cb4ec9498e6
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:37:55 +0000

    #92 New Tasks Language File:

---
+++ lang/en/tasks.php
@@ -0,0 +1,35 @@
+<?php 
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tasks Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for task-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Task',
+    'update' => 'Update Task',
+    'show' => 'View Task Details',
+    'delete' => 'Delete Task',
+    'list' => 'List of Tasks',
+    'title' => 'Title',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'assigned_to' => 'Assigned To',
+    'taskable_type' => 'Taskable Type',
+    'taskable_id' => 'Taskable ID',
+    'priority' => 'Priority',
+    'status' => 'Status',
+    'due_date' => 'Due Date',
+];
