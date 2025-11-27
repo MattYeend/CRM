6dbf11085bc917270063b9231a230cc41166e0d1
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:35:28 +0000

    #92 New Product Language File

---
+++ lang/en/products.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Products Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for product-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Product',
+    'update' => 'Update Product',
+    'show' => 'View Product Details',
+    'delete' => 'Delete Product',
+    'list' => 'List of Products',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'sku' => 'SKU',
+    'price' => 'Price',
+    'currency' => 'Currency',
+    'quantity' => 'Quantity',
+    'meta' => 'Meta Information',
+];
