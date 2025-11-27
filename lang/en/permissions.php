b8f10b8e2f28493e70e5ca619ee9885dc84680f4
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:17:05 +0000

    #92 New Permissions Language File

---
+++ lang/en/permissions.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Permissions Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for permission-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Permission',
+    'update' => 'Update Permission',
+    'show' => 'View Permission Details',
+    'delete' => 'Delete Permission',
+    'list' => 'List of Permissions',
+    'name' => 'Name',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'label' => 'Label',
+];
