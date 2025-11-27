53d6bbd477e1c490bb3e76037c9664b186e84e38
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:26:42 +0000

    #92 New Pipeline Language File

---
+++ lang/en/pipelines.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pipelines Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for pipeline-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+
+    'create' => 'Create a Pipeline',
+    'update' => 'Update Pipeline',
+    'show' => 'View Pipeline Details',
+    'delete' => 'Delete Pipeline',
+    'list' => 'List of Pipelines',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'is_default' => 'Is Default',
+];
