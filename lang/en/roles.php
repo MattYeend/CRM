44738bd26eea10ebc3e8ef7cd4dc9f4477878e75
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-11-27 12:31:26 +0000

    #92 New Roles Language File

---
+++ lang/en/roles.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Roles Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for role-related messages
+    | throughout the CRM application. You can modify these lines to suit your
+    | application's requirements.
+    |
+    */
+    
+    'create' => 'Create a Role',
+    'update' => 'Update Role',
+    'show' => 'View Role Details',
+    'delete' => 'Delete Role',
+    'list' => 'List of Roles',
+    'name' => 'Name',
+    'description' => 'Description',
+    'created_by' => 'Created By',
+    'created_at' => 'Created At',
+    'updated_by' => 'Updated By',
+    'updated_at' => 'Updated At',
+    'deleted_by' => 'Deleted By',
+    'deleted_at' => 'Deleted At',
+    'permissions' => 'Permissions',
+    'assigned_users' => 'Assigned Users',
+    'assign_permission' => 'Assign Permission',
+    'remove_permission' => 'Remove Permission',
+    'assign_user' => 'Assign User',
+    'remove_user' => 'Remove User',
+    'not_found' => 'No Roles Found',
+    'label' => 'Label',
+];
