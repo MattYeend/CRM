99cb9efdcf86917270f8e7d22a525ef104fd5838
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-01-31 11:04:05 +0000

    #121 Removal of register link

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { dashboard, login, register } from '@/routes';
+import { dashboard, login } from '@/routes';
 import { Head, Link } from '@inertiajs/vue3';
 </script>
 
@@ -29,12 +29,12 @@ import { Head, Link } from '@inertiajs/vue3';
                     >
                         Log in
                     </Link>
-                    <Link
+                    <!-- <Link
                         :href="register()"
                         class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
                     >
                         Register
-                    </Link>
+                    </Link> -->
                 </template>
             </nav>
         </header>
