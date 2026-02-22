80b676a01e10262fa05caeb4a2d28365ab2aa3f6
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-02-22 22:01:46 +0000

    #138 New clear permission cache command

---
+++ app/Console/Commands/ClearPermissionCacheCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
+
+class ClearPermissionCacheCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:clear-permission-cache-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Cache::flush();
+
+        $this->info('Permission cache cleared.');
+    }
+}
