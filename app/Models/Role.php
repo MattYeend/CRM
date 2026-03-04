b4f494e430d68f0fb09f61fece737d0ddef8e996
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-03-04 20:56:27 +0000

    #155 Minor update to role model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -31,7 +31,7 @@ class Role extends Model
      */
     public function users(): BelongsToMany
     {
-        return $this->belongsToMany(User::class)->withTimestamps();
+        return $this->belongsToMany(User::class);
     }
 
     /**
@@ -41,6 +41,6 @@ class Role extends Model
      */
     public function permissions(): BelongsToMany
     {
-        return $this->belongsToMany(Permission::class)->withTimestamps();
+        return $this->belongsToMany(Permission::class);
     }
 }
