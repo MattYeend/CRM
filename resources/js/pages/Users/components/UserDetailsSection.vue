dfb7224511c6a020397cf10abe2c3a45fdb0f7fd
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-03-17 08:13:58 +0000

    #169 Fix to failed test

--- resources/js/pages/Users/components/UserDetailsSection.vue
+++ resources/js/pages/Users/components/UserDetailsSection.vue
@@ -1,40 +1,53 @@
 <script setup lang="ts">
-    import UserRolesSelect from './UserRolesSelect.vue'
-    import UserJobTitlesSelect from './UserJobTitlesSelect.vue'
+import { ref, watch } from 'vue'
+import UserRolesSelect from './UserRolesSelect.vue'
+import UserJobTitlesSelect from './UserJobTitlesSelect.vue'
 
-    interface Role {
-        id: number
-        name: string
-    }
+interface Role {
+    id: number
+    name: string
+}
 
-    interface JobTitle {
-        id: number
-        title: string
-    }
-    
-    const { form, roles, jobTitles } = defineProps<{
-        form: any
-        roles: Role[]
-        jobTitles: JobTitle[]
-    }>()
+interface JobTitle {
+    id: number
+    title: string
+}
+
+const props = defineProps<{
+    form: any
+    roles: Role[]
+    jobTitles: JobTitle[]
+}>()
+
+const emit = defineEmits(['update:form'])
+
+const localForm = ref({ ...props.form })
+
+watch(
+    localForm,
+    (newVal) => {
+        emit('update:form', newVal)
+    },
+    { deep: true }
+)
 </script>
 
 <template>
     <div class="space-y-5">
         <div>
             <label class="block font-medium">Name</label>
-            <input v-model="form.name" class="border rounded w-full p-2"/>
+            <input v-model="localForm.name" class="border rounded w-full p-2"/>
         </div>
 
         <div>
             <label class="block font-medium">Email</label>
-            <input v-model="form.email" class="border rounded w-full p-2"/>
+            <input v-model="localForm.email" class="border rounded w-full p-2"/>
         </div>
 
         <div>
             <label class="block font-medium">Role</label>
             <UserRolesSelect 
-                v-model="form.role_id"
+                v-model="localForm.role_id"
                 :roles="roles"
             />
         </div>
@@ -42,7 +55,7 @@
         <div>
             <label class="block font-medium">Job Title</label>
             <UserJobTitlesSelect
-                v-model="form.job_title_id"
+                v-model="localForm.job_title_id"
                 :jobTitles="jobTitles"
             />
         </div>
--- resources/js/pages/Users/components/UserPasswordSection.vue
+++ resources/js/pages/Users/components/UserPasswordSection.vue
@@ -1,31 +1,33 @@
 <script setup lang="ts">
-    defineProps<{
-        form: any
-    }>()
+import { ref, watch } from 'vue'
+
+const props = defineProps<{
+    form: any
+}>()
+
+const emit = defineEmits(['update:form'])
+
+const localForm = ref({ ...props.form })
+
+watch(
+    localForm,
+    (newVal) => emit('update:form', newVal),
+    { deep: true }
+)
 </script>
 
 <template>
     <div class="space-y-5">
-        <h2 class="text-lg font-semibold">
-            Password
-        </h2>
+        <h2 class="text-lg font-semibold">Password</h2>
 
         <div>
             <label class="block font-medium">Password</label>
-            <input
-                type="password"
-                v-model="form.password"
-                class="border rounded w-full p-2"
-            />
+            <input type="password" v-model="localForm.password" class="border rounded w-full p-2"/>
         </div>
 
         <div>
             <label class="block font-medium">Confirm Password</label>
-            <input
-                type="password"
-                v-model="form.password_confirmation"
-                class="border rounded w-full p-2"
-            />
+            <input type="password" v-model="localForm.password_confirmation" class="border rounded w-full p-2"/>
         </div>
     </div>
 </template>
