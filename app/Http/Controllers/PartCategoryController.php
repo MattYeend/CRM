8840aca5d10e12ea87bce1dc712a407346db21e2
Matt Yeend <mattyeend1@hotmail.co.uk>
2026-03-25 14:55:00 +0000

    #255 Update to part category controller

--- app/Http/Controllers/PartCategoryController.php
+++ app/Http/Controllers/PartCategoryController.php
@@ -5,62 +5,177 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePartCategoryRequest;
 use App\Http\Requests\UpdatePartCategoryRequest;
 use App\Models\PartCategory;
+use App\Services\PartCategories\PartCategoryLogService;
+use App\Services\PartCategories\PartCategoryManagementService;
+use App\Services\PartCategories\PartCategoryQueryService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class PartCategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Declare a protected property to hold the PartCategoryLogService,
+     * PartCategoryManagementService and PartCategoryQueryService instance
+     *
+     * @var PartCategoryLogService
+     * @var PartCategoryManagementService
+     * @var PartCategoryQueryService
      */
-    public function index()
-    {
-        //
+    protected PartCategoryLogService $logger;
+    protected PartCategoryManagementService $management;
+    protected PartCategoryQueryService $query;
+
+    /**
+     * Constructor for the controller
+     *
+     * @param PartCategoryLogService $logger
+     *
+     * @param PartCategoryManagementService $management
+     *
+     * @param PartCategoryQueryService $query
+     *
+     * An instance of the PartCategoryLogService used for logging
+     * part-related actions
+     * An instance of the PartCategoryManagementService for management
+     * of parts
+     * An instance of the PartCategoryQueryService for the query of
+     * part-related actions
+     */
+    public function __construct(
+        PartCategoryLogService $logger,
+        PartCategoryManagementService $management,
+        PartCategoryQueryService $query,
+    ) {
+        $this->logger = $logger;
+        $this->management = $management;
+        $this->query = $query;
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
      */
-    public function create()
+    public function index(Request $request): JsonResponse
     {
-        //
+        $this->authorize('viewAny', PartCategory::class);
+
+        $part = $this->query->list($request);
+
+        return response()->json($part);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param StorePartCategoryRequest $request
+     *
+     * @return JsonResponse
      */
-    public function store(StorePartCategoryRequest $request)
+    public function store(StorePartCategoryRequest $request): JsonResponse
     {
-        //
+        $partCategory = $this->management->store($request);
+
+        $user = $request->user();
+
+        $this->logger->partCategoryCreated(
+            $user,
+            $user->id,
+            $partCategory,
+        );
+
+        return response()->json($partCategory, 201);
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param PartCategory $partCategory
+     *
+     * @return JsonResponse
      */
-    public function show(PartCategory $partCategory)
+    public function show(PartCategory $partCategory): JsonResponse
     {
-        //
-    }
+        $this->authorize('view', $partCategory);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(PartCategory $partCategory)
-    {
-        //
+        $partCategory = $this->query->show($partCategory);
+
+        return response()->json($partCategory);
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param UpdatePartCategoryRequest $request
+     *
+     * @param PartCategory $partCategory
+     *
+     * @return JsonResponse
      */
-    public function update(UpdatePartCategoryRequest $request, PartCategory $partCategory)
-    {
-        //
+    public function update(
+        UpdatePartCategoryRequest $request,
+        PartCategory $partCategory
+    ): JsonResponse {
+        $partCategory = $this->management->update($request, $partCategory);
+
+        $user = $request->user();
+
+        $this->logger->partCategoryUpdated(
+            $user,
+            $user->id,
+            $partCategory,
+        );
+
+        return response()->json($partCategory);
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param PartCategory $partCategory
+     *
+     * @return JsonResponse
      */
-    public function destroy(PartCategory $partCategory)
+    public function destroy(PartCategory $partCategory): JsonResponse
     {
-        //
+        $this->authorize('delete', $partCategory);
+
+        $user = auth()->user();
+
+        $this->logger->partCategoryDeleted(
+            $user,
+            $user->id,
+            $partCategory,
+        );
+
+        $this->management->destroy($partCategory);
+
+        return response()->json(null, 204);
+    }
+
+    /**
+     * Restore the specified resource from storage.
+     *
+     * @param int $id
+     *
+     * @return JsonResponse
+     */
+    public function restore(int $id): JsonResponse
+    {
+        $partCategory = PartCategory::withTrashed()->findOrFail($id);
+        $this->authorize('restore', $partCategory);
+        $this->management->restore((int) $id);
+
+        $user = auth()->user();
+
+        $this->logger->partCategoryRestored(
+            $user,
+            $user->id,
+            $partCategory,
+        );
+
+        return response()->json($partCategory);
     }
 }
